13badd6c92777229337c0befd80dfe62043e07aa
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2021-10-11 11:27:03 -0300

    added css

--- components/hero.tsx
+++ components/hero.tsx
@@ -18,8 +18,42 @@ import cn from 'classnames';
 import styleUtils from './utils.module.css';
 import styles from './hero.module.css';
 import { BRAND_NAME, DATE, SITE_DESCRIPTION } from '@lib/constants';
+import React, { useEffect, useState } from 'react';
+import creds from '../secrets.json';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 export default function Hero() {
+
+  const [bigUrl, setBigUrl] = useState('');
+
+  const typeEvent = (e: React.FormEvent<HTMLInputElement>) => {
+    setBigUrl(e.currentTarget.value);
+  }
+
+  useEffect(() => {
+
+    async function createUrl() {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      const doc = new GoogleSpreadsheet('12f_LRVjsk-6b9hyzIBJSe3lhaYlwn-7Y6EVHsS7_Uog');
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      await doc.useServiceAccountAuth(creds);
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      await doc.loadInfo();
+
+
+      const sheet = doc.sheetsByIndex[1];
+
+      console.log(sheet.title);
+    }
+
+
+    void createUrl().then();
+  });
+
   return (
     <div className={styles.wrapper}>
       <h2
@@ -48,7 +82,17 @@ export default function Hero() {
       </h2>
       <div className={cn(styleUtils.appear, styleUtils['appear-fourth'], styles.info)}>
         <p>
-          <strong>Online</strong>
+          <strong>And free url-shorter</strong>
+          <br />
+          <strong>Type your big url</strong>
+          <br />
+          <input className={styleUtils.myInput} id={'movie'} name={'movie'}
+                 required={true}
+                 placeholder={'Type your big url'}
+                 type={'text'} value={bigUrl}
+                 onInput={e => typeEvent(e)} />
+          <br/>
+          <button className={styleUtils.myButton}> Create Url</button>
         </p>
       </div>
     </div>
